836069041553a55d72a5f63f1cee9274f50d16e4
alexandrarozanova <aleksandra.rozan@mail.ru>
2024-05-31 16:46:12 +0700

    Добавлен обработчик событий в начало скрипта.

--- TemperatureConversion/script.js
+++ TemperatureConversion/script.js
@@ -1,25 +1,27 @@
-const celsiusTemperatureTextField = document.getElementById("celsius");
-const conversionButton = document.getElementById("conversion-button");
+document.addEventListener("DOMContentLoaded", function () {
+    const celsiusTemperatureTextField = document.getElementById("celsius");
+    const conversionButton = document.getElementById("conversion-button");
 
-celsiusTemperatureTextField.addEventListener("keyup", function () {
-    const celsiusTemperature = celsiusTemperatureTextField.value;
-    celsiusTemperatureTextField.classList.remove("invalid");
+    celsiusTemperatureTextField.addEventListener("keyup", function () {
+        const celsiusTemperature = celsiusTemperatureTextField.value;
+        celsiusTemperatureTextField.classList.remove("invalid");
 
-    if (isNaN(celsiusTemperature) || celsiusTemperature.length === 0) {
-        celsiusTemperatureTextField.classList.add("invalid");
+        if (isNaN(celsiusTemperature) || celsiusTemperature.length === 0) {
+            celsiusTemperatureTextField.classList.add("invalid");
 
-        conversionButton.disabled = true;
-        conversionButton.classList.remove("submit");
-    } else {
-        conversionButton.disabled = false;
-        conversionButton.classList.add("submit");
+            conversionButton.disabled = true;
+            conversionButton.classList.remove("submit");
+        } else {
+            conversionButton.disabled = false;
+            conversionButton.classList.add("submit");
 
-        conversionButton.addEventListener("click", function () {
-            const fahrenheitTemperatureTextField = document.getElementById("fahrenheit");
-            fahrenheitTemperatureTextField.value = (celsiusTemperature * 9 / 5) + 32;
+            conversionButton.addEventListener("click", function () {
+                const fahrenheitTemperatureTextField = document.getElementById("fahrenheit");
+                fahrenheitTemperatureTextField.value = (celsiusTemperature * 9 / 5) + 32;
 
-            const kelvinTemperatureTextField = document.getElementById("kelvin");
-            kelvinTemperatureTextField.value = (celsiusTemperature * 1) + 273.15;
-        });
-    }
+                const kelvinTemperatureTextField = document.getElementById("kelvin");
+                kelvinTemperatureTextField.value = (celsiusTemperature * 1) + 273.15;
+            });
+        }
+    });
 });
