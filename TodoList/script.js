f7dcb4a81f20645afe2557d103b0f7c4b40f2319
alexandrarozanova <aleksandra.rozan@mail.ru>
2024-05-31 20:15:45 +0700

    Добавлена задача TodoList.

---
+++ TodoList/script.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addTodoForm = document.getElementById("add-todo-form");
+    const todoList = document.getElementById("todo-list");
+    const newTodoTextField = document.getElementById("new-todo-text-field");
+
+    addTodoForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        let newTodoText = newTodoTextField.value.trim();
+        newTodoTextField.classList.remove("invalid");
+
+        if (newTodoText.length === 0) {
+            newTodoTextField.classList.add("invalid");
+            return;
+        }
+
+        const newTodo = document.createElement("li");
+        newTodo.classList.add("todo-item");
+
+        function setViewMode() {
+            newTodo.innerHTML = `
+                <span class="todo-item-text"></span>
+                <button class="delete-button" 
+                        type="button">Удалить</button>
+                <button class="edit-button"
+                        type="button">Редактировать</button>
+        `;
+
+            newTodo.querySelector(".todo-item-text").textContent = newTodoText;
+
+            newTodo.querySelector(".delete-button").addEventListener("click", function () {
+                newTodo.remove();
+            });
+
+            newTodo.querySelector(".edit-button").addEventListener("click", function () {
+                newTodo.innerHTML = `
+                    <input type="text" class="edit-text-field">
+                    <button class="cancel-button" 
+                        type="button">Отменить</button>
+                    <button class="save-button"
+                        type="button">Сохранить</button>
+            `;
+
+                const editTextField = document.querySelector(".edit-text-field");
+                editTextField.value = newTodoText;
+
+                newTodo.querySelector(".cancel-button").addEventListener("click", function () {
+                    setViewMode();
+                });
+
+                const errorMessage = document.createElement("span");
+                errorMessage.classList.add("error-message");
+                errorMessage.innerHTML = "Необходимо указать текст";
+
+                document.querySelector(".todo-item").append(errorMessage);
+
+                newTodo.querySelector(".save-button").addEventListener("click", function () {
+                    const changedTodoText = editTextField.value.trim();
+
+                    if (changedTodoText.length === 0) {
+                        editTextField.classList.add("invalid");
+                        return;
+                    }
+
+                    newTodoText = changedTodoText;
+                    setViewMode();
+                });
+            });
+        }
+
+        setViewMode();
+
+        todoList.append(newTodo);
+
+        newTodoTextField.value = "";
+    });
+});
