e4df4645a583b4341eed6675a9b61416197001fb
alexandrarozanova <aleksandra.rozan@mail.ru>
2024-06-08 14:31:19 +0700

    Запись цветов стала единообразной, исправлено использование селекторов только по тегу, добавлено отличие стилей при заблокированной кнопке.

--- TemperatureConversion/script.js
+++ TemperatureConversion/script.js
@@ -2,26 +2,40 @@ document.addEventListener("DOMContentLoaded", function () {
     const celsiusTemperatureTextField = document.getElementById("celsius");
     const conversionButton = document.getElementById("conversion-button");
 
+    let celsiusTemperature = celsiusTemperatureTextField.value;
+
+    const convertTemperature = function () {
+        conversionButton.disabled = false;
+        conversionButton.classList.add("enabled");
+
+        conversionButton.addEventListener("click", function () {
+            const fahrenheitTemperatureTextField = document.getElementById("fahrenheit");
+            fahrenheitTemperatureTextField.value = (Number(celsiusTemperature) * 9 / 5) + 32;
+
+            const kelvinTemperatureTextField = document.getElementById("kelvin");
+            kelvinTemperatureTextField.value = (Number(celsiusTemperature)) + 273.15;
+        });
+    };
+
+    if (celsiusTemperature === "") {
+        celsiusTemperatureTextField.value = 0;
+        convertTemperature();
+    }
+
     celsiusTemperatureTextField.addEventListener("keyup", function () {
-        const celsiusTemperature = celsiusTemperatureTextField.value;
+        celsiusTemperature = celsiusTemperatureTextField.value;
         celsiusTemperatureTextField.classList.remove("invalid");
 
         if (isNaN(celsiusTemperature) || celsiusTemperature.length === 0) {
             celsiusTemperatureTextField.classList.add("invalid");
 
             conversionButton.disabled = true;
-            conversionButton.classList.remove("submit");
+            conversionButton.classList.remove("enabled");
+            conversionButton.classList.add("disabled");
         } else {
-            conversionButton.disabled = false;
-            conversionButton.classList.add("submit");
-
-            conversionButton.addEventListener("click", function () {
-                const fahrenheitTemperatureTextField = document.getElementById("fahrenheit");
-                fahrenheitTemperatureTextField.value = (celsiusTemperature * 9 / 5) + 32;
-
-                const kelvinTemperatureTextField = document.getElementById("kelvin");
-                kelvinTemperatureTextField.value = (celsiusTemperature * 1) + 273.15;
-            });
+            conversionButton.classList.remove("disabled");
+            conversionButton.classList.add("enabled");
+            convertTemperature();
         }
     });
 });
